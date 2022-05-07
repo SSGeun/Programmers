a589c38dae5e6ac42f5133b0019423e45d44e0d9
SSGeun <ssg4778@naver.com>
2022-05-07 22:24:40 +0900

    Create 신고 결과 받기.cpp
    Level 1: 신고 결과 받기

---
+++ Level 1/신고 결과 받기.cpp
@@ -0,0 +1,91 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> split(string input, char delimiter);
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    
+    vector<int> answer;
+    
+    // answer 초기화
+    for (int i = 0; i < id_list.size(); i++) {
+        
+        answer.push_back(0);
+    }
+    
+    bool result[1001][1001] = { false };
+    
+    for (int i = 0; i < report.size(); i++) {
+        
+        int user_id;
+        int report_id;
+        
+        vector<string> report_split = split(report.at(i), ' ');
+        
+        for (int j = 0; j < id_list.size(); j++) {
+            
+            if (id_list.at(j) == report_split.at(0)) {
+                
+                user_id = j;
+                break;
+            }
+        }
+        
+        for (int k = 0; k < id_list.size(); k++) {
+            
+            if (id_list.at(k) == report_split.at(1)) {
+                
+                report_id = k;
+                break;
+            }
+        }
+        
+        result[user_id][report_id] = true;
+    }
+    
+    // 정지 기준이 되는 신고 횟수 k 확인
+    for (int i = 0; i < id_list.size(); i++) {
+        
+        int count = 0;
+        
+        vector<int> temp;
+        temp.clear();
+        temp.assign(answer.begin(), answer.end());
+        
+        for (int j = 0; j < id_list.size(); j++) {
+            
+            if (result[j][i] == true) {
+                
+                answer.at(j) += 1;
+                
+                count++;
+            } 
+        }
+        
+        if (count < k) {
+            
+            answer.clear();
+            answer.assign(temp.begin(), temp.end());
+        }
+    }
+    
+    return answer;
+}
+
+// String Split Function
+vector<string> split(string input, char delimiter) {
+    
+    vector<string> answer;
+    stringstream ss(input);
+    string temp;
+ 
+    while (getline(ss, temp, delimiter)) {
+        
+        answer.push_back(temp);
+    }
+ 
+    return answer;
+}
