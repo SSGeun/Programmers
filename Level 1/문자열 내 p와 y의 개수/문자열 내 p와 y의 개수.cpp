890439033f2283602fa4a5920c3774241c495ed9
SSGeun <ssg4778@naver.com>
2022-05-09 13:15:19 +0900

    Programmers Level 1
    Level 1: 문자열 내 p와 y의 개수

---
+++ Level 1/문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s) {
+    
+    bool answer = true;
+
+    int p_count = 0;
+    int y_count = 0;
+    
+    for (int i = 0; i < s.length(); i++) {
+        
+        if (s.at(i) == 'p' || s.at(i) == 'P') {
+            
+            p_count++;
+        }
+        
+        else if (s.at(i) == 'y' || s.at(i) == 'Y') {
+            
+            y_count++;
+        }
+    }
+    
+    if (p_count != y_count) {
+        
+        answer = false;
+    }
+
+    return answer;
+}
