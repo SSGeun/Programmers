53f13c57b98bfae1a65198fb7bc728fa62234a13
SSGeun <ssg4778@naver.com>
2022-09-30 20:55:21 +0900

    Programmers Level 0
    Level 0: 짝수의 합

---
+++ Level 0/짝수의 합/짝수의 합.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    
+    int answer = 0;
+    
+    for (int i = 2; i <= n; i++) {
+        
+        answer += ((i % 2) == 0) ? i : 0;
+    }
+    
+    return answer;
+}
